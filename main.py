5cf9068d3a0c51c71042dcd815062a365a54fb4e
alexandredchaves <xandechaves@gmail.com>
2023-10-18 14:11:08 -0300

    correcao do dicionario e add de arquivos

--- main.py
+++ main.py
@@ -1,10 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import repositorio
+
 
 app = Flask(__name__)
 @app.route("/")
 def home():
     dicionario = repositorio.retornar_personagens()
-    return render_template("personagem.html", dados = dicionario)
+    return render_template("personagem.html", dados=dicionario)
+
+@app.route("/test")
+def test():
+    dicionario = repositorio.retornar_personagens()
+    return dicionario
+
+@app.route("/personagem/<int:id>", methods=['GET', 'POST'])
+def editar_personagem(id):
+
+    if request.method == 'POST':
+        if "excluir" in request.form:
+            repositorio.remover_personagem(id)
+            return redirect(url_for("home"))
+        elif "salvar" in request.form:
+            personagem = {}
+            personagem['nome'] = request.form['nome']
+            personagem['cla'] = request.form['cla']
+            personagem['vila'] = request.form['vila']
+            personagem['ocupacao'] = request.form['ocupacao']
+            personagem['habilidades'] = request.form['habilidades']
+            personagem['imagem'] = request.form['imagem']
+
+            if id in repositorio.retornar_personagens().keys():
+                repositorio.atualizar_personagem(id, personagem)
+
+            return redirect(url_for("home"))
+    else:
+
+        personagem = repositorio.retornar_personagem(id)
+        personagem['id'] = id
+        return render_template("cadastro.html", **personagem)
+    
+@app.route("/personagem", methods=['GET', 'POST'])
+def criar_personagem():
+    if request.method == 'POST':
+            personagem = {}
+            personagem['nome'] = request.form['nome']
+            personagem['cla'] = request.form['cla']
+            personagem['vila'] = request.form['vila']
+            personagem['ocupacao'] = request.form['ocupacao']
+            personagem['habilidades'] = request.form['habilidades']
+            personagem['imagem'] = request.form['imagem']
+            repositorio.criar_personagem(**personagem)
+            return redirect(url_for("home"))
+
+    else:
+        return render_template("cadastro.html", id=repositorio.gerar_id())
 
 app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,50 @@
+# Projeto CRUD Básico
+
+Este é um projeto CRUD básico implementado utilizando as seguintes tecnologias:
+
+- Flask: Um framework web em Python.
+- Python: Linguagem de programação utilizada para desenvolver a aplicação.
+- HTML: Linguagem de marcação utilizada para estruturar as páginas da aplicação.
+- Bootstrap: Framework CSS utilizado para estilizar a aplicação.
+
+## Funcionalidades
+
+O projeto possui as seguintes funcionalidades:
+
+- Listar todos os itens
+- Exibir detalhes de um item
+- Adicionar um novo item
+- Editar um item existente
+- Excluir um item
+
+## Como executar o projeto
+
+Siga as instruções abaixo para executar o projeto localmente:
+
+1. Clone este repositório em sua máquina local.
+2. Certifique-se de ter o Python e o Flask instalados em seu ambiente.
+3. Navegue até o diretório do projeto no terminal.
+4. Execute o comando `python main.py` para iniciar o servidor Flask.
+5. Acesse a aplicação em seu navegador através do endereço `http://localhost:5000`.
+
+## Estrutura do diretório
+
+A estrutura do diretório do projeto é a seguinte:
+
+
+projeto-flask/
+  |- main.py
+  |- repositorio.py
+  |- templates/
+      |- index.html
+      |- cadastro.html
+      |- personagem.html
+      
+  
+
+
+O arquivo `repositorio.py` contém a lógica principal da aplicação Flask, enquanto os arquivos HTML na pasta `templates` são responsáveis por definir a estrutura das páginas. 
+
+## Contribuição
+
+Sinta-se à vontade para contribuir com este projeto abrindo uma issue ou enviando um pull request.
